30ffe77522313847ba4fe3638985afd19a766a88
Mansij Mishra <mansijm822@gmail.com>
2022-08-08 19:11:39 -0400

    Created connection string in appsetings, stored user and pass in secrets, and connected to server with a db context

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.Data.SqlClient;
+using SixMinAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var sqlConnBuilder = new SqlConnectionStringBuilder();
+
+sqlConnBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlConnBuilder.UserId = builder.Configuration["UserId"];
+sqlConnBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnBuilder.ConnectionString));
 
 var app = builder.Build();
 
